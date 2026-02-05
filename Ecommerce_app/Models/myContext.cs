6776ad906c91d1ae2d0f2f532f201b5fb40a727a
= <=>
2026-02-05 09:13:02 +0545

    Add project files.

---
+++ Ecommerce_app/Models/myContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Ecommerce_app.Models
+{
+    public class myContext : DbContext
+    {
+        public myContext(DbContextOptions<myContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Admin> tbl_admin { get; set; }
+    }
+}
